a60fb73ca68c649b54244a30d52f89a66760783c
neeraj1909 <neeraj1909@gmail.com>
2021-10-04 17:18:07 +0530

    implementation of lasso regression

---
+++ ch_4/lasso_regression.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import Lasso, SGDRegressor
+
+import numpy as np
+
+
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+
+ridge_reg = Lasso(alpha=1)
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+# using SGD (stochastic gradient descent)
+sgd_reg = SGDRegressor(penalty="l1")
+sgd_reg.fit(X, y.ravel())
+print(sgd_reg.predict([[1.5]]))
