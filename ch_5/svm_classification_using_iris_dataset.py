8e741940e2e8f72ed6862fc5fa7c4fd27bf97fa5
neeraj1909 <neeraj1909@gmail.com>
2021-10-08 14:27:03 +0530

    training a linear SVM model to detect Iris virginica flowers using Iris dataset

---
+++ ch_5/svm_classification_using_iris_dataset.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn import datasets
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
+
+iris = datasets.load_iris()
+X = iris["data"][:, (2, 3)]    # petal length, petal width
+y = (iris["target"] == 2).astype(np.float64)    # Iris virginica
+
+svm_clf = Pipeline([
+    ("scaler", StandardScaler()),
+    ("linear_svc", LinearSVC(C=1, loss="hinge")),
+])
+
+svm_clf.fit(X, y)
+
+print(svm_clf.predict([[5.5, 1.7]]))
+print(svm_clf.predict([[1.5, 1.7]]))
